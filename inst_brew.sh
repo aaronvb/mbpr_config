508f384da3eb23d2bd48fd4fa2c2e4bfdfc17ce8
Aaron Van Bokhoven <bokhoven@gmail.com>
2015-11-09 09:08:43 -1000

    Initial brew list

---
+++ inst_brew.sh
@@ -0,0 +1,22 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ack
+brew install awscli
+brew install ctags
+brew install elasticsearch
+brew install elixir
+brew install erlang
+brew install git-flow-avh
+brew install imagemagick
+brew install macvim
+brew install node
+brew install openssl
+brew install postgresql
+brew install qt5
+brew install rbenv
+brew install redis
+brew install tarsnap
+
+brew tap homebrew/science
+brew install gcc
+brew install r
